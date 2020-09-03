9c572938090ca55ba473efcee6baf1e75f3ac495
KING FTW <mouizghouri25@gmail.com>
2020-09-04 00:13:26 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,4 +22,8 @@ client.on('message', message => {
 
 });
 
-client.login(process.env.BOT_TOKEN);
+ 
+
+// THIS  MUST  BE  THIS  WAY
+
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
